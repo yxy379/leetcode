8808ec13677bbb69ef1ee39eaacc846a499e6d9f
yxy379 <ponny213@hotmail.com>
2019-03-05 14:20:25 -0600

    add solution for problem 223

--- math/223_rectangle area.cpp
+++ math/223_rectangle area.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,17 +9,18 @@ public:
 	int computeArea(int A, int B, int C, int D,
 		int E, int F, int G, int H)
 	{
-		for ()
-	}
-private:
-	bool inArea(int A, int B,
-		int E, int F, int G, int H)
-	{
-		if ((A > E) && (A < G) && (B > F) && (B < H))
-		{
-			return true;
-		}
-		else 
-			return false;
+		return (D - B) * (C - A) +
+			(G - E) * (H - F) -
+			max(0, (min(C, G) - max(A, E))) *
+			max(0, (min(D, H) - max(B, F)));
+
 	}
 };
+
+int main()
+{
+	Solution a;
+	cout << a.computeArea(-3, 0, 3, 4, 0, -1, 9, 2) << endl;
+	system("pause");
+	return 1;
+}
