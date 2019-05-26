afcdd3ed1b93ca27e7be0039c46a0e78c6487887
yxy379 <ponny213@hotmail.com>
2019-05-26 00:18:02 -0500

    add 040

--- back search/040_combination sum II.cpp
+++ back search/040_combination sum II.cpp
@@ -8,25 +8,27 @@ class Solution
 public:
 	vector<vector<int>> combinationSum(vector<int>& nums, int target)
 	{
-		vector<vector<int>> res;
 		vector<int> cur;
+		vector<vector<int>> res;
 		sort(nums.begin(), nums.end());
 		dfs(nums, target, 0, cur, res);
 		return res;
 	}
 private:
-	void dfs(vector<int>& nums, int target, int start, vector<int>& cur, vector<vector<int>>& res)
+	void dfs(vector<int>&nums, int target, int start, vector<int>& cur, vector<vector<int>>& res)
 	{
 		if (target < 0) return;
-		if (target == 0) 
+		if (target == 0)
 		{
-			res.push_back(cur); return;
+			res.push_back(cur);
+			return;
 		}
 
 		for (int i = start; i < nums.size(); i++)
 		{
+			if ((i > start) && (nums[i] == nums[i - 1])) continue;
 			cur.push_back(nums[i]);
-			dfs(nums, target - nums[i], i, cur, res);
+			dfs(nums, target - nums[i],  i + 1, cur, res);
 			cur.pop_back();
 		}
 	}
@@ -34,9 +36,10 @@ private:
 
 int main()
 {
+	vector<int> nums{ 10,1,2,7,6,1,5 };
+	int target = 8;
 	Solution a;
-	vector<int> nums{ 2, 3, 5 };
-	vector<vector<int>> res = a.combinationSum(nums, 8);
+	vector<vector<int>> res = a.combinationSum(nums, target);
 	for (int i = 0; i < res.size(); i++)
 	{
 		for (int j = 0; j < res[i].size(); j++)
